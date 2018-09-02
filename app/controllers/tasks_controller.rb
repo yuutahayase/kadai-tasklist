618a7b04b6a7fb95b59129811d462f9389bf1ee5
yuutahayase <yuta0203veru@gmail.com>
2018-09-02 12:22:01 +0000

    完成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy, :edit, :update, :create]
+  before_action :correct_user, only: [:destroy, :edit, :update]
 
   def edit
     @task
@@ -40,6 +40,7 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
